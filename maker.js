81efb8b332d094d8d7c926cb27e7c43592bd891d
Sapthesh V <sapthesh@users.noreply.github.com>
2020-08-19 18:46:23 +0530

    Update maker.js

--- maker.js
+++ maker.js
@@ -27,8 +27,8 @@ var html = `
     <script src="https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://raw.githubusercontent.com/LeeluPradhan/G-Index/master/css/mail-box-style.css" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://raw.githubusercontent.com/LeeluPradhan/G-Index/master/css/mail-box-style.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://raw.githubusercontent.com/sapthesh/GDrive-Index/tree/master/css/mail-box-style.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://raw.githubusercontent.com/sapthesh/GDrive-Index/tree/master/css/mail-box-style.css" crossorigin="anonymous">
 </head>
 <body style="background-image: url('Your Background Image');">
     <br>
